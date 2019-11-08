711903468dacb35f21013a85870a493c75a33805
jasonsun01 <v-jasonsun@hotmail.com>
2019-11-07 20:46:44 -0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            
+                int number = 0;
+                {
+                    // Assign the address of number to a pointer:
+                    int* p = &number;
+
+                    // Print the value of *p:
+                    System.Console.WriteLine("Value at the location pointed to by p: {0:X}", *p);
+
+                    // Print the address stored in p:
+                    System.Console.WriteLine("The address stored in p: {0}", (int)p);
+                }
+
+                System.Console.ReadKey();
+            
+
+
+
+
+
+
+            //// to get the input string
+            //Console.WriteLine("Please input servral number seperated by \'+\'");           
+            //String inputString = Console.ReadLine();         
+            //String[] inputSplit = inputString.Split('+');
+            //int result = 0;
+            //int paraMeter = 0;
+            //for (int i = 0; i <= inputSplit.Length-1; i++) 
+            //{
+            //      if(Int32.TryParse(inputSplit[i], out paraMeter))
+            //    {
+            //        result = result + paraMeter;
+            //    }
+
+            //}
+            //Console.WriteLine(string.Concat($"The result of the number sequence is:{result}"));
+        }
+    }
+   
+    }
+
+}
